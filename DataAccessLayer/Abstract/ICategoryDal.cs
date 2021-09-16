300d2f7052576cc1203a9368c713de7722bd1ec4
Cem <cicekcem25@gmail.com>
2021-09-16 21:17:03 +0300

    Add project

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> ListAllCategory();
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(Category category);
+        Category GetByID(int id);
+    }
+}
