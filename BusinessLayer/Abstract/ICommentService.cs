b894aa0158a1145dbb4df558dc47df628819c72c
Cem <cicekcem25@gmail.com>
2021-10-01 22:02:26 +0300

    degisikler

---
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICommentService
+    {
+        void CoomentAdd(Comment comment);
+        // void CoomentDelete(Comment comment);
+        // void CoomentUpdate(Category category);
+        List<Comment> GetList(int id);
+        // Comment GetById(int id);
+    }
+}
